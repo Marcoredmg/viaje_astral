4c163f58fe76ea3a0f048413753275e35c07dbce
Marcore <marcore.dmg@gmail.com>
2016-10-25 00:10:46 -0500

    agregado formulario de registro de viajes

--- travels/models.py
+++ travels/models.py
@@ -6,8 +6,12 @@ class Travel(models.Model):
 	name = models.CharField(max_length=255)
 	description = models.CharField(max_length=255)
 	price = models.DecimalField(max_digits=6, decimal_places=2)
-	image = models.ImageField(blank=True)
 	siteurl = models.CharField(max_length=255, blank=True)
+	image = models.ImageField(blank=True)
+	
 
 	def __str__(self):
 		return self.name
+
+	class Meta:
+		ordering = ('name',)
--- travels/views.py
+++ travels/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from .models import Travel
 from django.template import loader
+from .forms import TravelForm
 
 
 def hello_world(request):
@@ -19,3 +20,20 @@ def travel_detail(request, pk):
 		'travel': travel
 	}
 	return HttpResponse(template.render(context, request))
+
+
+def new_travel(request):
+	if request.method == 'POST':
+		form = TravelForm(request.POST, request.FILES)
+		if form.is_valid():
+			travel = form.save()
+			travel.save()
+			return HttpResponseRedirect('/')
+	else:
+		form = TravelForm()
+
+	template = loader.get_template('new_travel.html')
+	context = {
+		'form': form
+	}
+	return HttpResponse(template.render(context, request))	
