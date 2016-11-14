fb1c83e5403d425de3d0f65c4411694fd6393357
Marcore <marcore.dmg@gmail.com>
2016-11-13 21:48:30 -0600

    mejoras en diseño

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,14 @@
 function CalcularDescuento(){
 	precio_unitario = parseInt(document.getElementById('tbPrecio').value);
-	descuento = .15;
 	num_personas = document.getElementById('selectPersonas').value;
 	
 
-	if(num_personas == 3){
-		precio_final = precio_unitario - (precio_unitario * descuento);
+	if(num_personas == 5){
+		precio_final = precio_unitario - (precio_unitario * .20);
+		precioFinal.textContent = "$" + precio_final + " USD c/u"
+	}
+	if(num_personas == 10){
+		precio_final = precio_unitario - (precio_unitario * .30);
 		precioFinal.textContent = "$" + precio_final + " USD c/u"
 	}
 
